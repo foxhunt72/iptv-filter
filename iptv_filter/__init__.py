c2cbf9c69059a2c992e1e07ea44ceab36851691e
Richard de Vos <richard.de.vos@sentia.com>
2022-05-23 14:34:16 +0200

    initial changes

--- iptv_filter/__init__.py
+++
@@ -1,5 +0,0 @@
-# import os
-
-# # Prevent django reload thread from running config!
-# if os.environ.get('RUN_MAIN', None) != 'true':
-#     default_app_config = 'iptv_filter.apps.IptvFilterConfig'
